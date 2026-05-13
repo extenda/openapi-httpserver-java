4113fb94c2b84781a8275ba15d48697858777987
Thomas Cederholm <thomas.cederholm@extendaretail.com>
2026-05-13 08:59:43 +0200

    feat: Add ContentTypeHeader helper for parsing media-type headers

---
+++ src/test/java/com/retailsvc/http/internal/ContentTypeHeaderTest.java
@@ -0,0 +1,56 @@
+package com.retailsvc.http.internal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class ContentTypeHeaderTest {
+
+  @Test
+  void subtypeReturnsBareMediaType() {
+    assertThat(ContentTypeHeader.subtype("application/json")).isEqualTo("application/json");
+  }
+
+  @Test
+  void subtypeStripsParameters() {
+    assertThat(ContentTypeHeader.subtype("text/plain; charset=utf-8")).isEqualTo("text/plain");
+  }
+
+  @Test
+  void subtypeTrimsWhitespace() {
+    assertThat(ContentTypeHeader.subtype("  application/json  ")).isEqualTo("application/json");
+  }
+
+  @Test
+  void subtypeDefaultsToApplicationJsonWhenNull() {
+    assertThat(ContentTypeHeader.subtype(null)).isEqualTo("application/json");
+  }
+
+  @Test
+  void parameterReturnsValue() {
+    assertThat(ContentTypeHeader.parameter("text/plain; charset=iso-8859-1", "charset"))
+        .contains("iso-8859-1");
+  }
+
+  @Test
+  void parameterUnquotesValue() {
+    assertThat(ContentTypeHeader.parameter("text/plain; charset=\"utf-8\"", "charset"))
+        .contains("utf-8");
+  }
+
+  @Test
+  void parameterReturnsEmptyWhenMissing() {
+    assertThat(ContentTypeHeader.parameter("text/plain", "charset")).isEmpty();
+  }
+
+  @Test
+  void parameterNameMatchIsCaseInsensitive() {
+    assertThat(ContentTypeHeader.parameter("text/plain; CHARSET=utf-8", "charset"))
+        .contains("utf-8");
+  }
+
+  @Test
+  void parameterReturnsEmptyForNullHeader() {
+    assertThat(ContentTypeHeader.parameter(null, "charset")).isEmpty();
+  }
+}
